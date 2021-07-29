ab75b42c2382c2ce7dbaecd585e850290109dc32
HoangVu@pc <vuhoang1609@gmail.com>
2021-07-29 12:56:46 +0100

    Implement github api fetcher functions

---
+++ src/fetcher.js
@@ -0,0 +1,60 @@
+const { Octokit } = require("@octokit/rest");
+
+const octokit = new Octokit({
+    userAgent: 'repo-summary-workflow v1.0',
+    timeZone: 'Europe/London',
+    baseUrl: 'https://api.github.com',
+});
+
+
+const fetchRepository = async (owner, repo) => {
+
+    const response = await octokit.request('GET /repos/{owner}/{repo}', {
+        owner,
+        repo,
+    });
+
+    switch (response.status) {
+        case 404: {
+            throw `Repository ${owner}/${repo} not found.`;
+        }
+        case 403: {
+            throw `Unsufficient permission to view ${owner}/${repo}`;
+        }
+        case 301: {
+            throw `Repository ${owner}/${repo} has been moved permanently.`;
+        }
+    }
+
+    return response.data;
+}
+
+const fetchLanguageList = async (owner, repo) => {
+    const response = await octokit.request('GET /repos/{owner}/{repo}/languages', {
+        owner,
+        repo,
+    });
+
+
+    if (response.status != 200) {
+        throw 'Failed to fetch repository languages';
+    }
+
+    return response.data;
+}
+
+
+const getRepositoryInfo = async (owner, repo) => {
+    const basicData = await fetchRepository(owner, repo);
+    const languages = await fetchLanguageList(owner, repo);
+
+    basicData['languages'] = languages;
+
+    return basicData;
+}
+
+module.exports = {
+    fetchRepository,
+    fetchLanguageList,
+    getRepositoryInfo,
+}
