2f1089ede4a577f36ccb20f21b434a1f0f335a55
HoangVu@pc <vuhoang1609@gmail.com>
2021-08-12 11:55:39 +0100

    Update logging for setup.js

--- setup.js
+++ setup.js
@@ -1,3 +1,33 @@
 const { spawn } = require('child_process');
+const core = require('@actions/core');
+const fs = require("fs");
 
-spawn("curl", ["-O", "https://raw.githubusercontent.com/github/linguist/master/lib/linguist/languages.yml"]);
+const curl = spawn("curl", ["-O", "https://raw.githubusercontent.com/github/linguist/master/lib/linguist/languages.yml"]);
+
+if (curl.stdout) {
+    // Only needed for pipes
+    curl.stdout.on('data', function (data) {
+        core.info(data);
+    });
+}
+
+if (curl.stderr) {
+    // Only needed for pipes
+    curl.stderr.on('data', function (data) {
+        core.error(data);
+    });
+}
+
+app.on('close', (code) => {
+    if (code !== 0) {
+        core.error("Exit with status: " + code)
+        return reject({ code, outputData });
+    }
+
+    if (fs.existsSync("languages.yml")) {
+        core.info("File languages.yml has been downloaded!")
+    }
+    return resolve({ code, outputData });
+});
+
+app.on('error', (code) => reject({ code, outputData, errorData }));
