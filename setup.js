eb60fc9c91dfe77e93790e108832380cb545d4d5
HoangVu@pc <vuhoang1609@gmail.com>
2021-08-12 12:07:36 +0100

    Fix compile error

--- setup.js
+++ setup.js
@@ -1,5 +1,4 @@
 const { spawn } = require('child_process');
-const core = require('@actions/core');
 const fs = require("fs");
 
 const curl = spawn("curl", ["-O", "https://raw.githubusercontent.com/github/linguist/master/lib/linguist/languages.yml"]);
@@ -7,25 +6,25 @@ const curl = spawn("curl", ["-O", "https://raw.githubusercontent.com/github/ling
 if (curl.stdout) {
     // Only needed for pipes
     curl.stdout.on('data', function (data) {
-        core.info(data);
+        console.log(data);
     });
 }
 
 if (curl.stderr) {
     // Only needed for pipes
     curl.stderr.on('data', function (data) {
-        core.error(data);
+        console.error(data);
     });
 }
 
 app.on('close', (code) => {
     if (code !== 0) {
-        core.error("Exit with status: " + code)
+        console.error("Exit with status: " + code)
         return reject({ code, outputData });
     }
 
     if (fs.existsSync("languages.yml")) {
-        core.info("File languages.yml has been downloaded!")
+        console.log("File languages.yml has been downloaded!")
     }
     return resolve({ code, outputData });
 });
